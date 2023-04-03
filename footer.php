832d106d67df7ee7f2f7a45858c7f3179d593f7e
Fadli Ramadan <69451514+Freganovic@users.noreply.github.com>
2023-04-03 10:58:53 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>&copy; 2023, Teknik Informatika, <a href="https://wa.me/6285695335590" style="text-decoration: none;">Fadli Ramadan</a>, Universitas Pelita Bangsa</p>
+  </div>
+  </body>
+
+  </html>
